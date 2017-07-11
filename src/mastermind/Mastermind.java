3566cb3c2fd1630d9b61a5cb397f28ed0f58103f
james <james@138.119.196.53>
2017-07-11 14:54:02 -0300

    Added comments

--- src/mastermind/Mastermind.java
+++ src/mastermind/Mastermind.java
@@ -1,9 +1,12 @@
 package mastermind;
 
-/**
- *
- * @author james
- */
+//******************************************************************************
+//  Mastermind (or Cows and Bulls, since we aren't really using colours...)
+//  Crack a code within 10 attempts or else... you lose and can then try again.
+//  Date Created: July 10th 2017
+//  Last Updated: July 11th 2017
+//******************************************************************************
+
 public class Mastermind 
 {
 
